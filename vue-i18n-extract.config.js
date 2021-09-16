693a1bfaa70e1210891fb992b9c1802af77192da
theunclehonnor <jekbormoley@gmail.com>
2021-09-16 12:08:26 +0300

    feat: vue-i18n-extract config added

---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    vueFiles: './src/**/*.vue',
+    languageFiles: './src/**/*.?(en_GB|ru_RU).js',
+    output: false,
+    add: false,
+    remove: false,
+    ci: false,
+    separator: '.'
+}
